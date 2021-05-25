377ce2fc65c98446ab6b963b8af3e5ad976e32d2
trungpd1102 <letsmile95@gmail.com>
2021-05-25 13:55:29 +0700

    add Post List

--- HOOK/basic-hook/src/App.js
+++ HOOK/basic-hook/src/App.js
@@ -1,4 +1,6 @@
 import './App.scss';
+
+
 // import { useState } from 'react';
 
 // import ColorBox from './components/ColorBox/ColorBox';
@@ -8,8 +10,11 @@ function App() {
 
   return (
     <div className="app">
+      {/* <h1>React Hook - TodoList</h1>
+      <Todolist /> */}
+      <h1>React Hook - PostList</h1>
       <Todolist />
-    </div>
+     </div>
 
   );
 }
--- HOOK/basic-hook/src/components/ToDoList/Index.js
+++ HOOK/basic-hook/src/components/ToDoList/Index.js
@@ -1,63 +1,81 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 // import PropTypes from 'prop-types';
-import List from './TodoList'
-import TodoForm from './TodoForm'
+// import List from './TodoList'
+// import TodoForm from './TodoForm'
+import PostList from "../PostList/PostList";
 
-TodoList.propTypes = {
-
-};
+TodoList.propTypes = {};
 
 function TodoList(props) {
-    const [todoList, setTodoList] = useState([
-        { id: 0, title: 'Hello React Hook✨' },
-        { id: 1, title: 'This is basic React Hook🎇' },
-        { id: 2, title: 'Hope this one can help me improve my React skill ⌨' },
-    ])
-    // const [id, setId] = useState()
+	const [todoList, setTodoList] = useState([
+		{ id: 0, title: "Hello React Hook✨" },
+		{ id: 1, title: "This is basic React Hook🎇" },
+		{ id: 2, title: "Hope this one can help me improve my React skill ⌨" },
+	]);
 
-    // function handleTodoClick(todo) {
-    //     if (id && id !== todo.id) document.getElementById(id).style.color = '';
+	const [postList, setPostList] = useState([]);
 
-    //     document.getElementById(todo.id).style.color = 'blue';
-    //     setId(todo.id);
-    // }
+	useEffect(() => {
+		async function fetchPostList() {
+			try {
+				const requestUrl = `http://js-post-api.herokuapp.com/api/posts?_limit=5&_page=1`;
+				const response = await fetch(requestUrl);
+				const responseJSON = await response.json();
+				console.log({ responseJSON });
 
-    function handleTodoClick(todo) {
-        console.log(todo);
-        const index = todoList.findIndex(x => x.id === todo.id);
+				const { data } = responseJSON;
+				setPostList(data);
+			} catch (error) {
+				console.log("Failed to fetch data:", error.message);
+			}
+		}
 
-        if (index < 0) return;
+		fetchPostList();
+	}, []);
+	// const [id, setId] = useState()
 
-        let newTodoList = [...todoList];
-        newTodoList.splice(index, 1);
+	// function handleTodoClick(todo) {
+	//     if (id && id !== todo.id) document.getElementById(id).style.color = '';
 
-        setTodoList(newTodoList);
-    }
+	//     document.getElementById(todo.id).style.color = 'blue';
+	//     setId(todo.id);
+	// }
 
-    function handleTodoFormSubmit(formValue) {
-        console.log("Form values:", formValue);
-        // add new todo to list
-        const newTodo = {
-            id: todoList.length,
-            ...formValue,
-        }
-        const newTodoList = [...todoList];
-        
-        newTodoList.push(newTodo);
-        setTodoList(newTodoList)
-    }
+	function handleTodoClick(todo) {
+		console.log(todo);
+		const index = todoList.findIndex((x) => x.id === todo.id);
 
-    return (
-        <div>
-            <h1>React Hook - TodoList</h1>
+		if (index < 0) return;
 
-            <List
+		let newTodoList = [...todoList];
+		newTodoList.splice(index, 1);
+
+		setTodoList(newTodoList);
+	}
+
+	function handleTodoFormSubmit(formValue) {
+		console.log("Form values:", formValue);
+		// add new todo to list
+		const newTodo = {
+			id: todoList.length,
+			...formValue,
+		};
+		const newTodoList = [...todoList];
+
+		newTodoList.push(newTodo);
+		setTodoList(newTodoList);
+	}
+
+	return (
+		<div>
+			{/* <List
                 todos={todoList}
                 onTodoClick={handleTodoClick}
             />
-            <TodoForm onSubmit={handleTodoFormSubmit}/>
-        </div>
-    );
+            <TodoForm onSubmit={handleTodoFormSubmit}/> */}
+			<PostList posts={postList} />
+		</div>
+	);
 }
 
 export default TodoList;
